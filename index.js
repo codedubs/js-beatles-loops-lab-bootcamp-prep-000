e2422ce5f416373990793d005085da1d1977c741
Pitta <mailez4all@gmail.com>
2018-02-08 09:25:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,41 @@
+var musicians = [ "John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr" ];
+var instruments = [ "Guitar", "Bass Guitar", "Lead Guitar", "Drums" ];
+
+function theBeatlesPlay(musicians, instruments) {
+  var arrayNew = [ ];
+  for (var i = 0; i < musicians.length;
+        i++) {
+
+          arrayNew.push(`${musicians[i]} plays ${instruments[i]}`)
+
+        }
+  return arrayNew;
+}
+
+
+const facts = ["foo","bar"]
+
+function johnLennonFacts(facts) {
+
+  var i = 0;
+  var arr = [];
+
+  while (i < facts.length) {
+    arr.push(`${facts[i]}!!!`);
+    i += 1;
+  }
+
+  return arr;
+
+}
+
+function iLoveTheBeatles(i) {
+
+  var arr = [];
+
+  do {
+  console.log('I love the Beatles!');
+  } while (i < 15);
+
+
+}
